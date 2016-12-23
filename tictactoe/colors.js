88a84615608036389aac000c13ecc044165a6026
/ etharner </ etharner>
2016-12-24 03:01:53 +1000

    Changing round colors.

--- tictactoe/colors.js
+++ tictactoe/colors.js
@@ -4,4 +4,5 @@ const oColor = 0xA8D4E3;
 const xColor = 0xEDA1B8;
 const controlColor = fieldColor;
 const controlLineColor = 0xFFFFFF;
-const missingRoundColor = 0x666666;
+const missingRoundColor = 0x474646;
+const currentRoundColor = 0xCFCEAE;
