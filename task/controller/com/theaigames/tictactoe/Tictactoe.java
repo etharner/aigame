b7a81cf0e04732c228a5b1dfe0b00dffe2db0fdc
/ etharner </ etharner>
2017-01-09 01:08:14 +1000

    Removing dev mode from engine.

--- task/controller/com/theaigames/tictactoe/Tictactoe.java
+++ task/controller/com/theaigames/tictactoe/Tictactoe.java
@@ -96,10 +96,6 @@ public class Tictactoe extends AbstractGame {
 	public static void main(String args[]) throws Exception {
 		Tictactoe game = new Tictactoe();
 
-		game.DEV_MODE = false;
-		game.TEST_BOT = "java -cp /home/jim/workspace/tictactoe-starterbot/bin/ bot.BotStarter";
-		game.NUM_TEST_BOTS = 2;
-
 		game.setupEngine(args);
 		game.runEngine();
 	}
