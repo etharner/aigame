d2942ed3644007921be5858bf611ea29b0baf72c
/ etharner </ etharner>
2017-01-13 02:49:16 +1000

    Updating Windows batch script for proper multi-agent running.

--- task/sp_win.bat
+++ task/sp_win.bat
@@ -2,7 +2,7 @@ SET sp="spawner-bin\win32\sp.exe"
 SET java="C:\Program Files\Java\jdk1.8.0_111\bin\java.exe"
 SET engine="controller\bin"
 
-%sp% --json -sr=report.json -hr=1 -wl=30 -y=1 --separator=//^
- --// --controller --out=std %java% -cp %engine% com.theaigames.tictactoe.Tictactoe 2^
- --// --in=*1.stdout --out=*1.stdin %java% bot.BotStarter^
- --// --in=*1.stdout --out=*1.stdin %java% bot.BotStarter
+%sp% --out=std --separator=//^
+ --// --controller  %java% -cp %engine% com.theaigames.tictactoe.Tictactoe 2^
+ --// --in=*0.stdout --out=*0.stdin %java% bot.BotStarter^
+ --// --in=*0.stdout --out=*0.stdin %java% bot.BotStarter
