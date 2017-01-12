06d4c6d5858beaa040c213421f139e4a37fbd919
/ etharner </ etharner>
2017-01-13 04:00:01 +1000

    Adding out file copying to visualizer.

--- task/sp_win.bat
+++ task/sp_win.bat
@@ -2,7 +2,9 @@ SET sp="spawner-bin\win32\sp.exe"
 SET java="C:\Program Files\Java\jdk1.8.0_111\bin\java.exe"
 SET engine="controller\bin"
 
-%sp% --out=std --separator=//^
+%sp% --separator=//^
  --// --controller  %java% -cp %engine% com.theaigames.tictactoe.Tictactoe 2^
  --// --in=*0.stdout --out=*0.stdin %java% bot.BotStarter^
  --// --in=*0.stdout --out=*0.stdin %java% bot.BotStarter
+
+copy out.txt ..\tictactoe\
