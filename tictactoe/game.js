de1f58f45bd74693e64175aec5eedba3897e7ec4
Asijzar <Asijzar>
2017-01-12 18:13:50 -0800

    Changing move processing.

--- tictactoe/game.js
+++ tictactoe/game.js
@@ -1,12 +1,3 @@
-//out.js format
-//const match = `
-//[player 1 name]
-//[player 2 name]
-//[rounds count]
-//[list of [x y] moves each on new line]
-//[winner name]
-//`
-
 class Game {
   constructor(firstPlayer, secondPlayer, roundsCount, fields, moves, winner) {
     this.firstPlayer = firstPlayer;
@@ -15,16 +6,33 @@ class Game {
     this.fields = fields;
     this.moves = moves;
     this.winner = winner;
-    this.match = "";
   }
 }
 
 class Move {
   constructor(stringMove) {
-    this.x = parseInt(stringMove[1]);
-    this.y = parseInt(stringMove[3]);
+    let x = parseInt(stringMove[1]);
+    let y = parseInt(stringMove[3]);
+
+    const places = [
+        [0, 1, 2, 9, 10, 11, 18, 19, 20],
+        [3, 4, 5, 12, 13, 14, 21, 22, 23],
+        [6, 7, 8, 15, 16, 17, 24, 25, 26],
+        [27, 28, 29, 36, 37, 38, 45, 46, 47],
+        [30, 31, 32, 39, 40, 41, 48, 49, 50],
+        [33, 34, 35, 42, 43, 44, 51, 52, 53],
+        [54, 55, 56, 63, 64, 65, 72, 73, 74],
+        [57, 58, 59, 66, 67, 68, 75, 76, 77],
+        [60, 61, 62, 69, 70, 71, 78, 79, 80]
+    ]
+
+    const squareNum = places[x][y];
+
+    this.x = parseInt(squareNum / 9);
+    this.y = parseInt(squareNum - this.x * 9);
   }
 }
+
 
 function readTextFile(file) {
   var req = new XMLHttpRequest();
@@ -48,7 +56,7 @@ function parseMatch(matchFile) {
   fields = new Array(roundsCount + 1);
   fields[0] = new Array(9).fill(new Array(9).fill(0));
   moves = new Array(roundsCount + 1);
-  moves[0] = new Move(0, 0);
+  moves[0] = new Move("[0, 0]");
 
   for (let r = 1; r <= roundsCount; r++) {
     const move = new Move(lines[3 + r - 1]);
