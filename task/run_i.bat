fe65e331e63be11dfa8d5a56f1de899ee0cd7968
etharner <etharner@gmail.com>
2017-02-25 12:30:55 +1000

    Adding batch script for spawner interactor running.

---
+++ task/run_i.bat
@@ -0,0 +1,14 @@
+CALL compile_i.bat
+SET sp="spawner-bin\win32\sp.exe"
+SET java="C:\Program Files\Java\jdk1.8.0_112\bin\java.exe"
+SET engine="interactor\bin"
+SET chrome="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+SET visualizator="file:///C:/dev/aigame/tictactoe/index.html"
+
+::%java% -cp %engine% com.theaigames.tictactoe.Tictactoe
+%sp% --out=std --separator=// -hr=1 --out=nul -wl=30 -tl=1 -ml=256 -y=1^
+ --// --in=*1.stdout --out=*1.stdin %java% -cp %engine% com.theaigames.tictactoe.Tictactoe ^
+ --// %java% bot.BotStarter
+
+copy out.txt ..\tictactoe
+%chrome% %visualizator% --allow-file-access-from-files
